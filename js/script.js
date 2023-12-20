f4bce92d1ed7f339d23107f7a77207f157579773
xJustFox <dodydaversa@gmail.com>
2023-12-20 16:58:37 +0100

    [Fix] Fixed exercise

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            email: '',
             arrEmail:[],
         }
     },
     created() {
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                this.email = response.data.response;
+                let email = response.data.response;
 
-                this.arrEmail.push(this.email);
+                this.arrEmail.push(email);
             })
         }
     },
